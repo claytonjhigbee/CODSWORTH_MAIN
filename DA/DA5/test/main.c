19fbb94c18b1cae822e60c19530272183080cb33
Clayton Higbee <higbee@unlv.nevada.edu>
2019-04-25 22:46:28 -0700

    Design Assignment 5 initial upload

---
+++ DA/DA5/test/main.c
@@ -0,0 +1,18 @@
+/*
+ * test.c
+ *
+ * Created: 4/25/2019 10:21:48 PM
+ * Author : clayt
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
